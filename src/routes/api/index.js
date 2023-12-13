deb37508d429c68b9910c1b278c6dc199baf348c
NicolasLopezSTC <gclan149@gmail.com>
2023-12-12 22:32:45 -0600

    working again
    Signed-off-by: NicolasLopezSTC <gclan149@gmail.com>

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,9 +1,7 @@
 import { Router } from 'express'
 import basicAuth from 'express-basic-auth'
 
-import persons from './persons'
-import companies from './companies'
-import departments from './departments'
+
 import employees from './employees'
 
 const router = Router()
@@ -23,14 +21,6 @@ router.get('/', (req, res) => {
 
 
 
-
-
-
-
-
-router.use('/persons', persons)
-router.use('/companies', companies)
-router.use('/departments', departments)
 router.use('/employees', employees)
 
 export default router
