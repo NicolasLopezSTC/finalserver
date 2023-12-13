0fb7c9d43b53d37fcb73d9d6a31037e3218f1a51
NicolasLopezSTC <143395793+NicolasLopezSTC@users.noreply.github.com>
2023-12-12 20:56:55 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import './utils/dotenv'
+
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import helmet from 'helmet'
+import bodyParser from 'body-parser'
+
+import logger from './utils/logger'
+import router from './routes'
+//import { notFound, errorHandler } from './utils/errors'
+
+const port = Number(process.env.PORT) || 3000
+
+const app = express()
+
+
+app.use(morgan(process.env.MORGAN_LOG))
+app.use(cors({ origin: process.env.ORIGIN, exposedHeaders: ['x-total-count', 'x-total-pages'] }))
+app.use(helmet())
+app.use(bodyParser.json())
+
+app.use('/',router)
+
+//app.use(notFound)
+//app.use(errorHandler)
+
+app.listen(port, () =>
+  logger.info(`Application started at http://localhost:${port}`)
+)
